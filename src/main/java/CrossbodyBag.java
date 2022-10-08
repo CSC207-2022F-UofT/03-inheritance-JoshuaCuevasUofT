324669b67bf0d21717daeca0c28f84dbf086189b
joshua cuevas <joshua.cuevas@mail.utoronto.ca>
2022-10-08 18:51:03 -0400

    done oct 7

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -49,7 +49,7 @@ public class CrossbodyBag extends Bag{
     }
     @Override
     public String toString() {
-        return this.color + " Crossbody Bag with {" + this.getNumberOfStraps() + "} straps (" + this.getNumberOfContents() + " / " +
+        return this.color + " Crossbody Bag with " + this.getNumberOfStraps() + " straps (" + this.getNumberOfContents() + " / " +
                 this.capacity + ")";
     }
 }
